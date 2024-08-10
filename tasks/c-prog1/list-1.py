fda4ef59950bc86ff94deb4d7f32d835ef6197b7
Emmanuel N C Brito <code@yet1dev.com>
2024-08-09 21:18:03 -0300

    [feat] c-prog1 >> Add answer of question 5 of list-1

--- tasks/c-prog1/list-1.py
+++ tasks/c-prog1/list-1.py
@@ -5,23 +5,34 @@ print('\nQuestion 1')
 
 pi=3.14
 radius=float(input('  Radius: '))
-print(f'> Circle Area = {pi*radius**2}')
+print(f'  | Circle Area = {pi*radius**2}')
 
 #============================================
 print('\nQuestion 2')
 
 earning=float(input('  Earning/hour: '))
 hour=int(input('  Hours/month: '))
-print(f'> Earning/month = {hour*earning}')
+print(f'  | Earning/month = {hour*earning}')
 
 #============================================
 print('\nQuestion 3')
 
 F=float(input('  Farenheit: '))
-print(f'> Celcius = {5*(F-32)/9}')
+print(f'  | Celcius = {5*(F-32)/9}')
 
 #============================================
 print('\nQuestion 4')
 
 C=float(input('  Celcius: '))
-print(f'> Farenheit = {(9*C/5)+32}')
+print(f'  | Farenheit = {(9*C/5)+32}')
+
+#============================================
+print('\nQuestion 5')
+A=int(input('  A (Integer): '))
+B=int(input('  B (Integer): '))
+C=float(input('  C (Real): '))
+
+print(f'  | 2A * ½B = {(2*A)*(B/2)}')
+print(f'  | 3A + C = {3*A+C}')
+print(f'  | C³ = {C**3}')
+
