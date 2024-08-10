1e611f53fa860996c79a9f3c6b35ee9e59a39087
Emmanuel N C Brito <code@yet1dev.com>
2024-08-09 21:00:31 -0300

    [feat] c-prog1 >> Add answer of question 2 of list-1

--- tasks/c-prog1/list-1.py
+++ tasks/c-prog1/list-1.py
@@ -1,8 +1,18 @@
 #============================================
 #       PROGRAMING 1 COURSE - LIST 1
 #============================================
+print('\nQuestion 1')
+
 pi=3.14
-radius=float(input('Radius: '))
-print(f'Circle Area: {pi*radius**2}')
+radius=float(input('  Radius: '))
+print(f'  Circle Area = {pi*radius**2}')
+
+#============================================
+print('\nQuestion 2')
+
+earning=float(input('  Earning/hour: '))
+hour=int(input('  Hours/month: '))
+print(f'  Earning/month = {hour*earning}')
+
 #============================================
 
