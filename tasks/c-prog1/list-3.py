e8a19cff2fd0a0592f691654a46166eab52f9734
Emmanuel N C Brito <code@yet1dev.com>
2024-08-13 15:35:50 -0300

    [conf] c-prog1 >> Create basic structure of list-3

---
+++ tasks/c-prog1/list-3.py
@@ -0,0 +1,22 @@
+#============================================
+#       PROGRAMING 1 COURSE - LIST 2
+#============================================
+
+#============================================
+#       INTERACTION WITH THE QUESTIONS
+#============================================
+while True:
+    match int(input('\nQuestion: ')):
+        case 0:  break
+        case 1:  question_1()
+        case 2:  question_2()
+        case 3:  question_3()
+        case 4:  question_4()
+        case 5:  question_5()
+        case 6:  question_6()
+        case 7:  question_7()
+        case 8:  question_8()
+        case 9:  question_9()
+        case 10: question_10()
+        case 11: question_11()
+        case 12: question_12()
