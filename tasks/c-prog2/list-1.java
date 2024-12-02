1e58188c2352e5aa9d25f54460bccd847b2d5ed4
Emmanuel N C Brito <code@yet1dev.com>
2024-12-02 16:11:56 -0300

    [feat] c-prog2 >> Add basic structure to code questions in java

---
+++ tasks/c-prog2/list-1.java
@@ -0,0 +1,41 @@
+//===========================================
+//             PROBLEM SOLUTION
+//===========================================
+import java.util.*;
+
+public class Main {
+	public static void main(String[] str){
+		int number; 
+		Runnable[] Question = new Runnable[2];
+//===========================================	
+		Question[1] = () -> {			
+			// SOLUTION HERE
+		};
+
+
+//===========================================
+//        BASIC LOOP SELECT QUESTION
+//===========================================
+		while (true) {
+			number = my.read("Question Number: ").Int();
+			if (number == 0) { break; } else { Question[number].run(); };
+		}
+	}
+}
+
+//===========================================
+//               MY FUNCTIONS
+//===========================================
+class my {
+	static String OUT;
+	static void print(String msg){ System.out.print(msg); }
+
+	static String Str() { return OUT; }
+
+	static int Int() { return Integer.parseInt(OUT); }
+	static double Double() { return Double.parseDouble(OUT); }
+ 
+	static my out(String in){ OUT=in; return new my(); }
+	static my read(String msg) { print(msg); return out(new Scanner(System.in).nextLine()); }
+}
+
