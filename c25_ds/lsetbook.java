305d3f328e4b1807390ea464a2b04d4d6f8b39d9
Emmanuel N C Brito <code@yet1dev.com>
2025-03-08 13:44:46 -0300

    [work] c25_ds >> Add lvl 2 of lsetmap with general methods

--- c25_ds/lsetbook.java
+++ c25_ds/lsetbook.java
@@ -69,7 +69,62 @@ public abstract class LSet1_Support<T> extends LSet0_Base<T> {
 		return (LSetBook<T>) this;
 	}
 }
+//========================================================
+//              LSETMAP LEVEL 2 - GENERAL
+//========================================================
+@SuppressWarnings("unchecked")
+public abstract class LSet2_General<T> extends LSet1_Support<T> {
+	public LSetBook<T> pag(int... pag){
+		book.set(0, this.get(pag));
+		return (LSetBook<T>) this;
+	}
+	public LSetBook<T> New(int n){
+		for (int i = 0; i < n; i++) {
+			this.book.add(new HashSet<>());
+		} return (LSetBook<T>) this;
+	}
+	public LSetBook<T> rip(int n){
+		int size = book.size();
+		n = (n < size) ? n : size-1; 
+	
+		for (int i = 0; i < n; i++) {
+			book.remove(size-1-n);
+		} return (LSetBook<T>) this;
+	}
+//========================================================
+	public <G> LSetBook<T> set(String prop, G val){
+		for (T obj: book.get(0)) { ((LBox<T>) obj).set(prop, val); }
+		return (LSetBook<T>) this;
+	}
+	public T obj(int... pag){
+		Iterator<T> pages = this.get(pag).iterator();
+		return (pages.hasNext()) ? pages.next() : null;
+	}
+	public Set<T> get(int... pag){ 
+		Set<T> pages = new HashSet<>();
+		for (int num: pag){ pages.addAll(book.get(num)); }
+		return pages;
+ 	}
+	public LSetBook<T> del(int... pag){
+		Set<T> pages = this.get(pag);
+		for (int num: pag){ book.get(num).clear(); }
+		for (T obj: pages){ ((LBox) obj).rstIndex(); }
+		this.src.removeAll(pages);
+		book.get(0).removeAll(pages);
 
+		return (LSetBook<T>) this;
+	}
+	public LSetBook<T> add(Object... args){
+		Safe code = new Safe(()->{
+			LBox.repo((LSetBook<T>) this);
+			T instance = this.factory.newInstance(args);
+			this.src.add(instance);
+			LBox.repo(null);
+		});
+		code.except("Instancia de objeto falhou");
+		return (LSetBook<T>) this;
+  }
+}
 //========================================================
 //            LBOX - BASE FOR INTERNAL ELEMENTS
 //========================================================
