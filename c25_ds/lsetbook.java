a4220e228ba184d66b555a662374d62ed956c1fe
Emmanuel N C Brito <code@yet1dev.com>
2025-03-08 13:46:35 -0300

    [work] c25_ds >> Add lvl 3 of lsetmap with tools methods

--- c25_ds/lsetbook.java
+++ c25_ds/lsetbook.java
@@ -126,6 +126,35 @@ public abstract class LSet2_General<T> extends LSet1_Support<T> {
   }
 }
 //========================================================
+//               LSETMAP LEVEL 3 - TOOLS
+//========================================================
+@SuppressWarnings("unchecked")
+public abstract class LSet3_Tools<T> extends LSet2_General<T> {
+	public int qnt(int... pag){ return this.get(pag).size(); }
+
+	public int sum(String prop){ 
+		int sum = 0;
+		for (T obj: this.get(0)){ sum += ((int) ((LBox<T>) obj).get(prop)); }
+		return sum;
+	}
+//========================================================
+	public LSetBook<T> filter(Predicate<T> lambda){
+		Stream<T> result = book.get(0).stream().filter(lambda);
+		book.set(0,  result.collect(Collectors.toSet()) );
+		return (LSetBook<T>) this;
+	}
+	public LSetBook<T> forEach(Consumer<T> lambda){
+		book.get(0).stream().forEach(lambda);
+		return (LSetBook<T>) this;
+	}
+	public <R> Set<R> map(Function<T,R> lambda){
+		Stream<R> result = book.get(0).stream().map(lambda);
+		return result.collect(Collectors.toSet());
+	}
+	public T reduce(T start, BinaryOperator<T> lambda){
+		return book.get(0).stream().reduce(start, lambda);
+	}
+}
 //            LBOX - BASE FOR INTERNAL ELEMENTS
 //========================================================
 @SuppressWarnings("unchecked")
