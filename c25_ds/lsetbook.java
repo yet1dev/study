45ff5c7f7f68323853182945da212b8a6cc2ac96
Emmanuel N C Brito <code@yet1dev.com>
2025-03-08 13:49:03 -0300

    [feat] c25_ds >> Add final lsetmap lvl with constructor/logic methods

--- c25_ds/lsetbook.java
+++ c25_ds/lsetbook.java
@@ -155,6 +155,42 @@ public abstract class LSet3_Tools<T> extends LSet2_General<T> {
 		return book.get(0).stream().reduce(start, lambda);
 	}
 }
+//========================================================
+//                   LSETMAP FINAL
+//========================================================
+@SuppressWarnings("unchecked")
+public class LSetMap<T> extends LSet3_Tools<T> implements LSetBook<T> {
+	public LSetMap(Class<T> type) {
+		this.type = type;
+		this.factory = (Constructor<T>) type.getConstructors()[0];
+	}
+//========================================================
+	public LSetBook<T> to(int... pag){  return send(this.book.get(0), pag); }
+	public LSetBook<T> rst(int... pag){ return send(new HashSet<>(), pag); }
+	public LSetBook<T> all(int... pag){ return send(this.src, pag); }
+	
+	public LSetBook<T> inv(int... pag){
+		Set<T> inv = new HashSet<>(src);
+		inv.removeAll(book.get(0));
+		return send(inv, pag);
+	}
+//========================================================
+	public <G> LSetBook<T> pin(String prop, G val){ return pick(0b0011, prop, val); }
+	public <G> LSetBook<T> not(String prop, G val){ return pick(0b1100, prop, val); }
+	
+	public <G> LSetBook<T> sel(String prop, G val){ return pick(0b0010, prop, val); }
+	public <G> LSetBook<T> mix(String prop, G val){ return pick(0b0111, prop, val); }
+	public <G> LSetBook<T> alt(String prop, G val){ return pick(0b0101, prop, val); }
+	public <G> LSetBook<T> cut(String prop, G val){ return pick(0b0100, prop, val); }
+	public <G> LSetBook<T> lim(String prop, G val){ return pick(0b0001, prop, val); }
+
+	public <G> LSetBook<T> sep(String prop, G val){ return pick(0b1101, prop, val); }
+	public <G> LSetBook<T> out(String prop, G val){ return pick(0b1000, prop, val); }
+	public <G> LSetBook<T> eqv(String prop, G val){ return pick(0b1010, prop, val); }
+	public <G> LSetBook<T> esc(String prop, G val){ return pick(0b1011, prop, val); }
+	public <G> LSetBook<T> ign(String prop, G val){ return pick(0b1110, prop, val); }
+}
+//========================================================
 //            LBOX - BASE FOR INTERNAL ELEMENTS
 //========================================================
 @SuppressWarnings("unchecked")
