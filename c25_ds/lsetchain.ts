fb0acec30989b607a13206b830abd5c2bc904549
Emmanuel N C Brito <code@yet1dev.com>
2025-03-17 15:29:31 -0300

    [rule] c25_ds >> Add the LSetChain interface with all mandatory methods

--- c25_ds/lsetchain.ts
+++ c25_ds/lsetchain.ts
@@ -12,3 +12,29 @@ export class Ride {       // EXEMPLE CLASS
 		this.name = name;
 	}
 }
+//==============================================================
+//                    ABSTRACT DATA TYPE
+//==============================================================
+export interface LSetChain<T> {
+	res(): any;
+	all(): Set<T>;
+	cur(): Set<T>;
+
+	reg(obj:T): LSetChain<T>;
+	add(...args: any[]): LSetChain<T>;
+
+	val(prop:string, uaib:number): any[];
+	obj(prop:string, val:any, uaib:number): T;
+	get(prop:string, val:any, uaib:number): Set<T>;
+	qnt(prop:string, val:any, uaib:number): number;
+	has(prop:string, val:any, uaib:number): boolean;
+
+	del(prop:string, val:any, uaib:number): LSetChain<T>;
+	set(prop:string, val:any, uaib:number): LSetChain<T>;
+	sel(prop:string, val:any, uaib:number): LSetChain<T>;
+
+	map(lambda:any): LSetChain<T>;
+	filter(lambda:any): LSetChain<T>;
+	forEach(lambda:any): LSetChain<T>;
+	reduce(start:T, lambda:any): LSetChain<T>;
+}
