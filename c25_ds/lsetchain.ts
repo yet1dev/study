0db9a9d3a9ee6ecc37f3f84c10aff594a944581f
Emmanuel N C Brito <code@yet1dev.com>
2025-03-17 15:10:38 -0300

    [test] c25_ds >> Add start typescript with Ride class to test the code

---
+++ c25_ds/lsetchain.ts
@@ -0,0 +1,14 @@
+//=============================================================
+//                   LOGICAL SET CHAIN
+//=============================================================
+export class Ride {       // EXEMPLE CLASS
+	private speed: string;
+	private color: string;
+	private name: string;
+
+	constructor(speed:string, color:string, name:string) {
+		this.speed = speed;
+		this.color = color;
+		this.name = name;
+	}
+}
