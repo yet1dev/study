5f339f9de28aaf5ff6e6ab2f7a556103c7bd382b
Emmanuel N C Brito <code@yet1dev.com>
2025-03-17 23:54:34 -0300

    [feat] c25_ds >> Add lvl0 of LSetMap with properties and proxy handler

--- c25_ds/lsetchain.ts
+++ c25_ds/lsetchain.ts
@@ -38,3 +38,24 @@ export interface LSetChain<T> {
 	forEach(lambda:any): LSetChain<T>;
 	reduce(start:T, lambda:any): LSetChain<T>;
 }
+//==============================================================
+//                   LSET0 BASIC COMPONENTS
+//==============================================================
+abstract class LSet0_Base<T> {
+	protected factory: any;
+	protected list = new Set<T>();
+	protected chain = new Set<T>();
+	protected result: any;
+	protected index = new Map<string, Map<any, Set<T>>>();
+
+	protected handler = {
+		set(obj: any, prop: string, val: any, proxy:any) {
+			let old = obj[prop];             // store old value
+			obj.repo.drop(proxy, prop, old); // del obj of old index[prop,val]
+			obj.repo.save(proxy, prop, val); // add obj in new index[prop,val]
+
+			obj[prop] = val; // update property value
+			return true;
+		},
+	};
+}
