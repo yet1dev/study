68545dd2494a03b06456835f56e4e20a28c352a5
Emmanuel N. C. Brito <code@yet1dev.com>
2025-06-16 14:14:40 -0300

    [rule] c25_theory >> Add basic type verifications

--- c25_theory/regex.py
+++ c25_theory/regex.py
@@ -1,17 +1,26 @@
 #==============================================================
 #                       REGEX LIBRARY
 #==============================================================
-def istype(expr, t, limit=True):
-  if isinstance(expr, t) and limit:
-    return expr
-  raise TypeError(f"Expect {t.__name__}, but receive {type(expr).__name__}")
+def handleType(args, t, lenArgs="*", lenItem="*"):
+  error = f"Expect {t.__name__}[{lenItem}], but receive"
+
+  if not isinstance(args, list):
+    raise TypeError(f"{error} {type(args).__name__}")
+  if not all(isinstance(item, t) for item in args):
+    raise TypeError(f"{error} some item not is {t}")
+  if (lenArgs != "*") and len(args) != lenArgs:
+    raise TypeError(f"{error} {len(args)} elements")
+  if (lenItem != "*") and not all(len(item) == lenItem for item in args):
+    raise TypeError(f"{error} a item with size different of {lenItem}")
+
+  return args
 
 #==============================================================
 class Regex: pass
 
 class RChar(Regex):
   def __init__(self, val1):
-    self.val1 = istype(val1, str, len(val1)==1)
+    self.val1 = handleType(val1, str, len(val1)==1)
 
 class Regex1(Regex):
   def __init__(self, val1):
@@ -29,5 +38,6 @@ class RStar(Regex1): pass
 
 class ROr(Regex2): pass
 class RAnd(Regex2): pass
-class RConcat(Regex2): pass # 
+class RConcat(Regex2): pass
+class RRange(Regex0): pass
 
